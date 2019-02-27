dcd6486917cb3bf2e3f2288f5c3d5ed599648dcb
Aleksandar Gjoreski <aj0715@live.com>
2019-02-27 19:59:37 +0100

    changed dbas from mlab to mongodb atlas

--- app.js
+++ app.js
@@ -20,24 +20,24 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.use(cookieSession({
-    maxAge: 24*60*60*1000, //in milliseconds (1 day here)
-    keys:[keys.session.cookieKey] //key to encrypt data in the cookie
+    maxAge: 24 * 60 * 60 * 1000, //in milliseconds (1 day here)
+    keys: [keys.session.cookieKey] //key to encrypt data in the cookie
 }));
 
 //initialaze passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-//connect to mongodb
-mongoose.connect(keys.mongoDB.dbURI,{ useNewUrlParser: true },()=>{
+// db connection
+mongoose.connect(keys.mongoDB.dbURI,{ useNewUrlParser: true, dbName: 'intersecDB' },()=>{
     console.log('connected to mongodb');
 })
 
-//set up routes
-app.use('/auth',authRoutes);
-app.use('/api',apiRoutes);
 
-app.listen(5000, ()=>{
+//set up routes
+app.use('/auth', authRoutes);
+app.use('/api', apiRoutes);
+
+app.listen(5000, () => {
     console.log('app listening for requests on port 5000');
 })
